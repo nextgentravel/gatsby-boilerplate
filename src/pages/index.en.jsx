493b10009c00cb5982980a20e74547d0cec366cf
msarauer <mitchelsarauer@hotmail.com>
2021-12-01 15:11:48 -0700

    Formatting change

--- src/pages/index.en.jsx
+++ src/pages/index.en.jsx
@@ -4,11 +4,11 @@ import Header from "../components/Header"
 
 const IndexPage = () => {
   return (
-<main id="main-content" aria-labelledby="h2-label">
-              <div className="container mt-4">
-                Content goes here.
-              </div>
-            </main>
+    <main id="main-content" aria-labelledby="h2-label">
+      <div className="container mt-4">
+        Content goes here.
+      </div>
+    </main>
   )
 }
 
